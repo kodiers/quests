20bbc324684221d03cf4522202e6812d73974fc6
kodiers <kodiers@gmail.com>
2015-11-06 16:04:48 +0300

    Fixed Bug #147
    http://ns.it-national.com/redmine/issues/147
    Added modal window, then event just created and have no tasks

--- static/js/mask_duration.js
+++ static/js/mask_duration.js
@@ -1,6 +1,8 @@
 /**
  * Created by kodiers on 05/11/15.
+ * This script add mask to duration field and shows showsCongrats modal.
  */
 $(document).ready(function () {
     $('#id_duration').mask('0d 00:00:00');
+    $('#showCongrats').modal('show');
 })
